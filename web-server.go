80a6b308f38714e875424205a8ed60e4cbb45f97
Travis Simon <tsimon@gmail.com>
2016-01-28 09:38:43 +1100

    Unknown changes

--- web-server.go
+++ web-server.go
@@ -7,16 +7,29 @@ import (
 
 var homeTemplate, _ = template.ParseFiles("web-view.html")
 
-type Page struct {
-    Title string
+type StringInfo struct {
+	Val string
+	Length string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	pg := &Page{Title: "Microservices Test"}
+type Page struct {
+    Title string
+	Info *StringInfo
+}
+
+func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	pg := &Page{Title: "Microservices Test", Info: nil}
 	homeTemplate.Execute(w, pg)
 }
 
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	pg := &Page{Title: "String info:", Info: nil}
+	homeTemplate.Execute(w, pg)
+}
+
+
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/", homepageHandler)
+    http.HandleFunc("/info/", infoHandler)
     http.ListenAndServe(":8080", nil)
 }
